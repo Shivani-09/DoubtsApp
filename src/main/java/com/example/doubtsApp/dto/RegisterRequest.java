40c1233fd344e27a3eb4543fbc8e1fa147434cdf
Shivani <ramidishihvani@gmail.com>
2025-07-28 22:32:25 +0530

    Verifying pull annd push into issue

--- src/main/java/com/example/doubtsApp/dto/RegisterRequest.java
+++ src/main/java/com/example/doubtsApp/dto/RegisterRequest.java
@@ -4,7 +4,7 @@ import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
 import lombok.Data;
-
+//just to verify pull & push
 @Data
 public class RegisterRequest {
     @NotBlank(message = "Username cannot be blank")
